5af38854555f915d9c11913a20d8f5d244ec742e
rodrik <rodrigo.ijk@gmail.com>
2017-12-12 10:23:43 +1300

    Assertion hint

--- complete/src/test/java/hello/WebMockTest.java
+++ complete/src/test/java/hello/WebMockTest.java
@@ -29,6 +29,10 @@ public class WebMockTest {
     public void greetingShouldReturnMessageFromService() throws Exception {
         when(service.greet()).thenReturn("Hello Mock");
         this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello Mock")));
+                .andExpect(content().string(containsString("Hello Mock")))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").isNotEmpty())
+                		;
     }
+    
+    
 }
